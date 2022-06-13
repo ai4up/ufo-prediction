74a151ba1b7bcdd1f41b99735893d7deb0407be4
Florian Nachtigall <nachtigall.florian@gmail.com>
2022-06-13 20:13:55 +0200

    Send slack notification when model training finishes
    * update remote url of cluster-utils submodule
    * remove unused ufo-map submodule

--- bin/train.py
+++ bin/train.py
@@ -4,6 +4,7 @@ import os
 import sys
 import time
 import logging
+import datetime
 
 PROJECT_ROOT = os.path.realpath(os.path.join(__file__, '..', '..'))
 PROJECT_SRC = os.path.join(PROJECT_ROOT, 'src')
@@ -17,15 +18,19 @@ from xgboost import XGBRegressor
 import preprocessing as pp
 from prediction_age import AgePredictor
 import cluster_utils.dataset as cluster_dataset
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+import cluster_utils.slack_notifications as slack
 
 COUNTRY = 'spain'
 N_CITIES = 4000
 CITIES = []
 DATA_DIR = '/p/projects/eubucco/data/2-database-city-level-v0_1'
 RESULT_DIR = '/p/tmp/floriann/ml-training'
+
+start_time = time.time()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+slack_channel = os.environ.get('SLACK_CHANNEL')
+slack_token = os.environ.get('SLACK_TOKEN')
 
 logger.info('Extracting features...')
 df = cluster_dataset.load(country_name=COUNTRY, path=DATA_DIR, cities=CITIES, n_cities=N_CITIES)
@@ -42,3 +47,10 @@ logger.info('Saving model...')
 timestr = time.strftime('%Y%m%d-%H-%M-%S')
 model_path = f'{RESULT_DIR}/model-{COUNTRY}-{N_CITIES or len(CITIES)}-{timestr}.pkl'
 predictor.save(model_path)
+
+logger.info('Sending slack notification...')
+try:
+    duration = str(datetime.timedelta(seconds=time.time() - start_time)).split('.')[0]
+    slack.send_message(f'Model training for {COUNTRY} finished after {duration}. 🚀', slack_channel, slack_token)
+except Exception as e:
+    logger.error(f'Failed to send Slack message: {e}')
